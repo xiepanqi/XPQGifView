5e015584e260495cfe040fe9cac71e1bc080cf69
xiepanqi <385833293@qq.com>
2016-03-19 11:14:29 +0800

    Initial Commit

---
+++ XPQGifViewDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XPQGifViewDome
+//
+//  Created by 谢攀琪 on 16/3/19.
+//  Copyright © 2016年 谢攀琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XPQGifViewDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XPQGifViewDome
+//
+//  Created by 谢攀琪 on 16/3/19.
+//  Copyright © 2016年 谢攀琪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
